d5d595b5e16ec0d2b5b77b0a90c62f44b17b27a6
okcma <1263982003@qq.com>
2017-01-12 13:56:06 +0800

    修改了显示周几的功能

--- app/src/main/java/com/mawanjun/mindakebiao/activity/CourseFragment.java
+++ app/src/main/java/com/mawanjun/mindakebiao/activity/CourseFragment.java
@@ -1,6 +1,7 @@
 package com.mawanjun.mindakebiao.activity;
 
 import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +27,7 @@ import com.mawanjun.mindakebiao.utils.ToastUtil;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.zip.Inflater;
 
@@ -40,7 +43,7 @@ import java.util.zip.Inflater;
 public class CourseFragment extends BaseFragment {
 
     private static final int REQUEST_LOGIN = 0;
-    private static final String [] TITLE_DATA = {"9月","周一","周二","周三","周四","周五","周六","周日"};
+ //   private static final String [] TITLE_DATA = {"9月","周一","周二","周三","周四","周五","周六","周日"};
     private static final int GRID_ROW_COUNT = 12;
     private static final int GRID_COL_COUNT = 8;
     private List<Course> mStuCourseList = new ArrayList<>();
@@ -50,6 +53,7 @@ public class CourseFragment extends BaseFragment {
     private GridLayout mGlClsTitle;
     private GridLayout mGlClsContent;
     private  ProgressDialog dialog;
+    LinearLayout weekNames;
   //  private TextView text;
     private int mTableDistance;
 //    private static CourseFragment mInstance;// 单例模式
@@ -74,8 +78,9 @@ public class CourseFragment extends BaseFragment {
         super.onCreate(savedInstanceState);
         View view = inflater.inflate(R.layout.course_fragment,container,false);
       //  text = (TextView) view.findViewById(R.id.text);
-       mGlClsTitle = (GridLayout) view.findViewById(R.id.main_grid_title);
+     //  mGlClsTitle = (GridLayout) view.findViewById(R.id.main_grid_title);
         mGlClsContent = (GridLayout) view.findViewById(R.id.main_grid_content);
+        weekNames = (LinearLayout) view.findViewById(R.id.weekNames);
        // button = (Button) view.findViewById(R.id.load_course);
         initWidget();
         processLogic();
@@ -100,7 +105,8 @@ public class CourseFragment extends BaseFragment {
         mTableDistance = getScreenPixelWidth()/15;
         mCourseService = new CourseService();
         mStuCourseDao = StuCourseDao.getInstance(getContext());
-        setUpClsTitle();
+    //   setUpClsTitle();
+        initWeekNameView();
         setUpClsContent();
 
 
@@ -122,31 +128,115 @@ public class CourseFragment extends BaseFragment {
         showCls();
         dialog.dismiss();
     }
-    //设置表格显示星期的地方
-    private void setUpClsTitle(){
-        for (int i=0; i<TITLE_DATA.length; ++i){
-            String content = TITLE_DATA[i];
-            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-            //第一列的时候
-            if (i == 0){
-                params.width = mTableDistance;
+    /**
+     * 顶部周一到周日的布局
+     **/
+    private void initWeekNameView() {
+        for (int i = 0; i < 7 + 1; i++) {
+            TextView tvWeekName = new TextView(getContext());
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            lp.gravity = Gravity.CENTER | Gravity.CENTER_HORIZONTAL;
+            if (i != 0) {
+                lp.weight = 1;
+                tvWeekName.setText("周" + intToZH(i));
+                if (i == getWeekDay()) {
+                    tvWeekName.setTextColor(Color.parseColor("#FF0000"));
+                } else {
+                    tvWeekName.setTextColor(Color.parseColor("#4A4A4A"));
+                }
+            } else {
+                lp.weight = 0.8f;
+                tvWeekName.setText(getMonth() + "月");
             }
-            else {
-                //添加分割线
-                LayoutInflater inflater=LayoutInflater.from(getContext());
-                View divider = inflater.inflate(R.layout.grid_title_form,mGlClsTitle,false);
-                mGlClsTitle.addView(divider);
-
-                params.width = mTableDistance * 2;
-            }
-            params.height = GridLayout.LayoutParams.MATCH_PARENT;
-            TextView textView = new TextView(getContext());
-            textView.setTextColor(getResources().getColor(R.color.blue));
-            textView.setText(content);
-            textView.setGravity(Gravity.CENTER);
-            mGlClsTitle.addView(textView,params);
+            tvWeekName.setGravity(Gravity.CENTER_HORIZONTAL);
+            tvWeekName.setLayoutParams(lp);
+            weekNames.addView(tvWeekName);
         }
     }
+
+    /**
+     * 当前星期
+     */
+    public int getWeekDay() {
+        int w = Calendar.getInstance().get(Calendar.DAY_OF_WEEK) - 1;
+        if (w <= 0) {
+            w = 7;
+        }
+        return w;
+    }
+    /**
+     * 当前月份
+     */
+    public int getMonth() {
+        int w = Calendar.getInstance().get(Calendar.MONTH) + 1;
+        return w;
+    }
+
+    /**
+     * 数字转换中文
+     */
+    public static String intToZH(int i) {
+        String[] zh = {"零", "一", "二", "三", "四", "五", "六", "七", "八", "九"};
+        String[] unit = {"", "十", "百", "千", "万", "十", "百", "千", "亿", "十"};
+
+        String str = "";
+        StringBuffer sb = new StringBuffer(String.valueOf(i));
+        sb = sb.reverse();
+        int r = 0;
+        int l = 0;
+        for (int j = 0; j < sb.length(); j++) {
+            r = Integer.valueOf(sb.substring(j, j + 1));
+            if (j != 0)
+                l = Integer.valueOf(sb.substring(j - 1, j));
+            if (j == 0) {
+                if (r != 0 || sb.length() == 1)
+                    str = zh[r];
+                continue;
+            }
+            if (j == 1 || j == 2 || j == 3 || j == 5 || j == 6 || j == 7 || j == 9) {
+                if (r != 0)
+                    str = zh[r] + unit[j] + str;
+                else if (l != 0)
+                    str = zh[r] + str;
+                continue;
+            }
+            if (j == 4 || j == 8) {
+                str = unit[j] + str;
+                if ((l != 0 && r == 0) || r != 0)
+                    str = zh[r] + str;
+                continue;
+            }
+        }
+        if (str.equals("七"))
+            str = "日";
+        return str;
+    }
+
+//    //设置表格显示星期的地方
+//    private void setUpClsTitle(){
+//        for (int i=0; i<TITLE_DATA.length; ++i){
+//            String content = TITLE_DATA[i];
+//            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+//            //第一列的时候
+//            if (i == 0){
+//                params.width = mTableDistance;
+//            }
+//            else {
+//                //添加分割线
+//                LayoutInflater inflater=LayoutInflater.from(getContext());
+//                View divider = inflater.inflate(R.layout.grid_title_form,mGlClsTitle,false);
+//                mGlClsTitle.addView(divider);
+//
+//                params.width = mTableDistance * 2;
+//            }
+//            params.height = GridLayout.LayoutParams.MATCH_PARENT;
+//            TextView textView = new TextView(getContext());
+//            textView.setTextColor(getResources().getColor(R.color.blue));
+//            textView.setText(content);
+//            textView.setGravity(Gravity.CENTER);
+//            mGlClsTitle.addView(textView,params);
+//        }
+//    }
 
 
 
